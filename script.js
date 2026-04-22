4d319d6fd2902716b8cfbbd840c43b504c5668d1
aivankhirlpastor <st25480@ormiston.school.nz>
2026-04-22 16:13:23 +1200

    Added for Interactivity later on + Defining Swiper

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+// Swipers
+const cuisineSwiper = new Swiper('#cuisine-swiper', {
+    direction: "horizontal",
+    effect: "slide",
+
+    slidesPerView: 3,
+    spaceBetween: 50,
+    loop: true,
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev"
+    // }
+});
