8f7dd6bb4a0063b600785feef9230dbae28f8473
aivankhirlpastor <st25480@ormiston.school.nz>
2026-04-24 23:07:22 +1200

    Implemented Panel Pop-Up Functionality

---
+++ script2.js
@@ -0,0 +1,229 @@
+// functions
+const makeUpCompliments = (id) => {
+    switch (id) {
+        case "e-30489":
+            return [
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Delightful",
+                },
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Tasty",
+                },
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Child-Friendly",
+                }
+            ];
+        case "e-64308":
+            return [
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Delicious",
+                },
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Healthy",
+                },
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Flavourful",
+                }
+            ];
+        case "e-10031":
+            return [
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Enjoyable",
+                },
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Nourishing",
+                },
+                {
+                    icon: `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="2"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path d="M12 20L4.3314 12.0474C3.47892 11.1633 3 9.96429 3 8.71405C3 6.11055 5.03517 4 7.54569 4C8.75128 4 9.90749 4.49666 10.76 5.38071L12 6.66667L13.24 5.38071C14.0925 4.49666 15.2487 4 16.4543 4C18.9648 4 21 6.11055 21 8.71405C21 9.96429 20.5211 11.1633 19.6686 12.0474L15.8343 16.0237" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </g>
+                            </svg>`,
+                    special: "Versatile",
+                }
+            ];
+    }
+}
+
+// This is for food review only.
+const makeUpParagraph = (id) => {
+    // an ID of a specific element to match with these followings:
+    switch (id) {
+        case "e-30489":
+            return `Raspberry pancake features a handy fresh morning session, non-ordinary regular pancake but with soft and fluffy texture in a twist of flavourful raspberries. These are becoming the highest priority for another exceptional yet delightful experience of every morning. Look at the bright pancakes, so gorgeous!`;
+        case "e-64308":
+            return `paragraph`;
+    }
+}
+
+const popBox = (img, rating, context, compliments) => {
+    // 1) Creating Board Panel
+    
+    // defining variables for main panel by creating the new element
+    const popPanel = document.createElement("div");
+    popPanel.classList.add("pop-box-panel");
+    // close btn
+    const closeButton = document.createElement("button");
+    closeButton.type = "button";
+    closeButton.classList.add("close-btn", "no-appearance-btn");
+    // icon
+    const closeIcon = document.createElement("img");
+    closeIcon.src = "./icons/x-symbol-svgrepo-com.svg";
+    closeIcon.alt = "A close icon, resembling the letter X";
+    closeButton.appendChild(closeIcon) // append close icon to close button
+    // overlay semi-transparent
+    const overlayDrop = document.createElement("div");
+    overlayDrop.classList.add("overlay-drop");
+    console.log(overlayDrop);
+
+    // 2) compliment making procedures
+    let numeralStamp = 0;
+    let fullListCompliment = "";
+
+    // 3) Gather each compliments into legible HTML structured list encapsulated to unordered list,
+    //    but requires to verify if it have lists of compliments, which avoids throwing the error.
+    if (compliments) {
+        compliments.forEach((co) => {
+            numeralStamp++;
+            fullListCompliment += `
+                <li class="card-compliment" id="cpt-${numeralStamp}">
+                    ${co.icon ? co.icon : "<svg><!-- Blank SVG --></svg"}
+                    <!-- Text Compliments: Special, Brief -->
+                    <span class="special">${co.special}</span>
+                </li>`;
+        })
+    }
+
+    // 4) adding necessary event listeners
+    closeButton.addEventListener("click", () => {
+        popPanel.remove();
+        overlayDrop.remove();
+    })
+
+    overlayDrop.addEventListener("click", () => {
+        popPanel.remove();
+        overlayDrop.remove();
+    })
+
+    // 5) creating based-structure HTML
+    popPanel.innerHTML += `
+        <!-- (I) Food Image Content -->
+        <img src="${img.src}" alt="${img.alt}" class="fixed-asp">
+        <!-- (II) Container Wraps -->
+        <div class="container-wraps">
+            <div class="food-text-description">
+                <h2>${context.heading.textContent}</h2>
+                <span class="list-rating">${rating.textContent}</span>
+                <!-- Compliment Wrapper -->
+                <ul class="compliment-container">
+                ${fullListCompliment}
+                </ul>
+                <p>${context.par.textContent}</p>
+            </div>
+            <button type="button" class="emphasis-btn">Write a Review...</button>
+        </div>
+        <!-- (III) Absolute Close Button -->
+    `;
+
+    // 6) It's appending time
+    // appending button (comes for the second of the last)
+    popPanel.appendChild(closeButton);
+    
+    // appending button (comes for the last after the button)
+    document.querySelector("body").appendChild(overlayDrop);
+
+    // appending the panel
+    document.querySelector("body").appendChild(popPanel);
+}
+
+
+// obtain informations for each of food cards
+document.querySelectorAll(".food-card").forEach((e) => {
+    // get a 4th child element from this parent (MUST BE BUTTON)
+    const viewBtn = e.children[3];
+    console.log(e.id);
+
+    // img
+    let foodImg = e.children[0];
+    let rating = e.children[1];
+    let textDscp = {
+        heading: e.children[2].children[0],
+        par: document.createElement("p")
+    };
+
+    // slight change
+    textDscp.par.textContent = makeUpParagraph(e.id);
+
+    // make up compliments
+    const foodCompliment = makeUpCompliments(e.id);
+
+    // verify (type such as div, button, h2, etc...)
+    if (viewBtn.type === "button") {
+        // add the eventListener for each of the btns
+        viewBtn.addEventListener("click", (e) => {
+            // img, heading, context
+            popBox(foodImg, rating, textDscp, foodCompliment);
+        });
+    }
+
+
+    // button
+})
